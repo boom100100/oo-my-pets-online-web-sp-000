3a9461718478d6f2621c369c83b208ebfc18d684
Bernadette Davis <bsd231@nyu.edu>
2019-10-03 18:27:26 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,11 +3,14 @@ class Cat
   attr_accessor :owner, :mood
   #attr_writer
   attr_reader :name
+  @@all
 
   def initialize(name = "", owner = "", mood = "nervous")
     @name = name
     @owner = owner
     @mood = mood
+    @@all << self
+    self
   end
 
 
