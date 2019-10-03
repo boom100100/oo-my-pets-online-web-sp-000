a7c4978b9ff863df58ff434faa237e6d3be161cf
Bernadette Davis <bsd231@nyu.edu>
2019-10-03 18:16:43 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,9 @@
 class Owner
   # code goes here
+  attr_accessor :name, :species
+
+  def say_species
+  end
   def buy_cat
   end
 
@@ -8,7 +12,9 @@ class Owner
 
   def buy_dog
   end
-  
+
   def  sell_dog
   end
+
+  def walk_dog
 end
