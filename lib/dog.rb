1997d7c1ffee1c4caba9e16c079044cb824347d6
Bernadette Davis <bsd231@nyu.edu>
2019-10-03 18:29:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,19 @@
 class Dog
   # code goes here
-  attr_accessor
-  attr_writer
-  attr_reader
+  attr_accessor :owner, :mood
+  #attr_writer
+  attr_reader :name
+  @@all = []
+
+  def initialize(name = "", owner = "", mood = "nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+    @@all << self
+    self
+  end
+
+  def self.all
+    @@all
+  end
 end
